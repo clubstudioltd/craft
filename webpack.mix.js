545006eb378d66d32633c3cf0e47f24046b8ceb5
Scott Wakefield <scott@clubstudio.co.uk>
2018-06-06 09:55:59 +0100

    Add Critical CSS, LoadJS and LoadCSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,21 @@
 let mix = require('laravel-mix');
 
+require('laravel-mix-criticalcss');
+
 mix.setPublicPath('web/')
    .js('resources/assets/js/app.js', 'web/js')
    .sass('resources/assets/sass/main.scss', 'web/css')
-   .version();
+   .version()
+   .criticalCss({
+        enabled: mix.inProduction(),
+        paths: {
+            base: process.env.SITE_URL,
+            templates: 'resources/templates/_includes/critical-css/'
+        },
+        urls: [
+            { url: '/', template: 'index' },
+        ],
+        options: {
+            minify: true,
+        },
+    });
