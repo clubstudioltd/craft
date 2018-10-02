a7f6a0969070a1cd451e6bea531ab55857b8afa6
Scott Wakefield <scott@clubstudio.co.uk>
2018-10-02 15:42:56 +0100

    Use Tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,15 @@
 let mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
 
 require('laravel-mix-criticalcss');
 
 mix.setPublicPath('web/')
    .js('resources/assets/js/app.js', 'web/js')
    .sass('resources/assets/sass/main.scss', 'web/css')
+   .options({
+      processCssUrls: false,
+      postCss: [ tailwindcss('./tailwind.js') ],
+   })
    .version()
    .criticalCss({
         enabled: mix.inProduction(),
