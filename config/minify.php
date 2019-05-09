c36769f63bf9683192dd61fa027d2ea1f8c931f7
Scott Wakefield <scott@clubstudio.co.uk>
2019-05-09 16:45:06 +0100

    Add minify plugin

---
+++ config/minify.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    // if set to `true` then Minify will not minify anything
+    "disableTemplateMinifying" => false,
+
+    // if set to `true` then Minify will not minify anything if `devMode` is enabled
+    "disableDevModeMinifying" => true,
+
+];
