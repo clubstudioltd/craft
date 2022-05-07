6b7a1907fa729990038892cc4846ea9f3fcf4109
Scott Wakefield <scott@clubstudio.co.uk>
2022-05-07 16:21:42 +0100

    Tailwind 3 and clean-up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 module.exports = {
-  purge: {
-    content: [
-      './resources/templates/**/*.twig',
-    ],
-    options: {
-      whitelist: [],
-    }
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './resources/templates/**/*.twig',
+  ],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,13 @@
-let mix = require('laravel-mix'),
-    tailwindcss = require('tailwindcss');
+let mix = require('laravel-mix');
 
 require('laravel-mix-criticalcss');
 
 mix.setPublicPath('web/')
     .js('resources/js/app.js', 'web/js')
-    .sass('resources/sass/main.scss', 'web/css')
+    .postCss('resources/css/main.css', 'web/css', [
+        require("tailwindcss"),
+    ])
     .copy('resources/img', 'web/img')
-    .options({
-        processCssUrls: false,
-        postCss: [ tailwindcss('./tailwind.config.js') ],
-    })
     .version()
     .criticalCss({
         enabled: mix.inProduction(),
